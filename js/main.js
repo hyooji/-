35d316054359821bb848d3f15ef21759b49d36ad
hyooji <107169893+hyooji@users.noreply.github.com>
2022-11-13 01:05:02 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@
 				enabled:true
 			},
 			zoom: {
-				enabled: true, // By default it's false, so don't forget to enable it
+				enabled: false, // By default it's false, so don't forget to enable it
 
 				duration: 300, // duration of the effect, in milliseconds
 				easing: 'ease-in-out', // CSS transition easing function
